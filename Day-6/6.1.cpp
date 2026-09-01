1636986e802bbae6b98153d177c765c1e6d31d01
Saso-24 <zaid_212@outlook.es>
2026-09-01 07:33:34 -0700

    6.1 is added

--- Day-6/6.1.cpp
+++ Day-6/6.1.cpp
@@ -1,6 +1,18 @@
-include <iostream>
+#include <iostream>
+using namespace std;
+
+class cat{
+	public:
+		unsigned short int age;
+		unsigned short int weight;
+		void meow();
+};
 
 int main (){
-	
-	return 0
+	cat pal;
+	pal.age = 2;
+	pal.weight = 20;
+	cout << "Pal is a " << pal.age << " years old cat\n";
+	cout << "He is " << pal.weight << "kg fat cat\n";
+	return 0;
 }
